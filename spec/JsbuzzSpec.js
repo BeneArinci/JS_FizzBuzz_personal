69b18f7b8bca2330734bf1a090a59d0651c495ca
Bene Arinci <benedetta.arinci@gmail.com>
2020-06-08 23:51:48 +0100

    completed. 6 tests running

--- spec/JsbuzzSpec.js
+++ spec/JsbuzzSpec.js
@@ -14,4 +14,24 @@ describe('JavaBuzz', function() {
         expect(javabuzz._isDivisibleBy(1, 3)).toBe(false);
       });
     });
+    describe('when play, game', function(){
+      it('returns "Java" if number divisible by 3', function(){
+        expect(javabuzz.game(6)).toEqual("Java");
+      });
+    });
+    describe('when play, game', function(){
+      it('returns "Buzz" if number is divisible by 5', function(){
+        expect(javabuzz.game(10)).toEqual("Buzz");
+      });
+    });
+    describe('when play, game', function(){
+      it('returns "JavaBuzz" if number is divisible by 3 and 5', function(){
+        expect(javabuzz.game(30)).toEqual("JavaBuzz")
+      });
+    });
+    describe('when play, game', function(){
+      it('returns the number when this is not divisible by 3 or 5', function(){
+        expect(javabuzz.game(8)).toEqual(8)
+      });
+    });
 } );
--- src/Jsbuzz.js
+++ src/Jsbuzz.js
@@ -1,7 +1,32 @@
 class JavaBuzz {
     _isDivisibleBy(number, divisor) {
         return(number % divisor === 0);
-
     }
+
+    isDivisibleByFifteen(number) {
+      return(this._isDivisibleBy(number,15));
+    }
+
+    isDivisibleByFive(number) {
+      return(this._isDivisibleBy(number,5));
+    }
+
+    isDivisibleByThree(number) {
+      return(this._isDivisibleBy(number,3));
+    }
+
+    game(number) {
+      if (this.isDivisibleByFifteen(number)) {
+        return "JavaBuzz";
+      }
+      if (this.isDivisibleByThree(number)) {
+        return "Java";
+      }
+      if (this.isDivisibleByFive(number)) {
+        return "Buzz";
+      }
+      return number;
+    }
+
 
 }
